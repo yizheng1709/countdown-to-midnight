1378230994b3b446252443bb5dce863f34092cca
Eva <yi.zheng1709@gmail.com>
2021-01-12 13:15:02 -0500

    commit tree 834e7a6c78b6a292cb4de3742687fc11816f40d2

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+    until seconds == 0
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+    end 
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+    until seconds == 0
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+        sleep(1.0)
+    end 
+    "HAPPY NEW YEAR!"
 end
